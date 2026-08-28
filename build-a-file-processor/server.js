f4f575ffefd1f4243a8c279c51bb0a3ea73071c8
MBWorkShop <319869787+MBWorkShop@users.noreply.github.com>
2026-08-28 17:28:40 +0000

    save

--- build-a-file-processor/server.js
+++ build-a-file-processor/server.js
@@ -1,1 +1,7 @@
-// Starter file — add your code here
+const fs = require('fs'); console.log(fs);
+fs.writeFileSync("assets/output.txt", "Hello, freeCodeCamp!");
+fs.appendFileSync("assets/output.txt", "\nICh bin Matti und los gehts!");
+const exists = fs.existsSync("assets/output.txt");
+console.log(exists);
+const entries = fs.readdirSync("assets");
+console.log(entries);
