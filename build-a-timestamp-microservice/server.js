ed1216b15e9ea7d3b2fafe9d62371ec9c3e6b034
MBWorkShop <319869787+MBWorkShop@users.noreply.github.com>
2026-09-06 16:34:16 +0000

    Complete timestamp microservice

--- build-a-timestamp-microservice/server.js
+++ build-a-timestamp-microservice/server.js
@@ -13,6 +13,41 @@ app.get("/", (_req, res) => {
 
 // Do not change code above this line
 
+const timestampHandler = (req, res) => {
+    const { date } = req.params; // URL-Parameter als String extrahieren
+
+    const timestamp = Number(date); // Versucht, den String in eine Zahl umzuwandeln; bei Nicht-Zahlen entsteht NaN
+
+    let parsedDate; // zunächst undefined, wird später mit einem Date-Objekt gefüllt
+
+    // Prüfen, ob der URL-String eine Zahl enthält.
+    // Falls ja, wird er als Unix-Timestamp interpretiert.
+    // Andernfalls wird er als Datumsstring interpretiert. (kann Objekt mit "Invalid Date" erzeugen)
+
+    if (!Number.isNaN(timestamp)) {
+        parsedDate = new Date(timestamp);
+    } else {
+        parsedDate = new Date(date);
+    }
+
+    // Prüfen, ob das erzeugte Date-Objekt ein gültiges Datum enthält
+
+    if (Number.isNaN(parsedDate.getTime())) {
+        return res.json({ error: "Invalid Date" });
+    }
+
+    res.json({
+        unix: parsedDate.getTime(),
+        utc: parsedDate.toUTCString()
+    });
+};
+
+
+app.get("/api", timestampHandler);
+app.get("/api/:date", timestampHandler);
+
+
+
 // Do not change code below this line
 
 const PORT = 8000;
